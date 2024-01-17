14d94c37a30eee50b46dadf1223fe5a6fefb5652
SerhiiMW <137871447+SerhiiMW@users.noreply.github.com>
2024-01-17 14:49:49 +0200

    homework-4

--- src/components/MyPhone/MyPhone.jsx
+++ src/components/MyPhone/MyPhone.jsx
@@ -1,44 +1,34 @@
-import { Component } from "react";
-import { nanoid } from "nanoid";
+import { useState, useEffect, useRef } from 'react';
+import { nanoid } from 'nanoid';
 
-import MyPhoneForm from "./MyPhoneForm/MyPhoneForm";
-import MyPhoneList from "./MyPhoneList/MyPhoneList";
+import MyPhoneForm from './MyPhoneForm/MyPhoneForm';
+import MyPhoneList from './MyPhoneList/MyPhoneList';
 
-import styles from "./my-phone.module.css";
+import styles from './my-phone.module.css';
 
-class MyPhone extends Component {
-    state = {
-        contacts: [
-            {
-                id: nanoid(),
-                name: 'Rosie Simpson',
-                number: '459-12-56'
-            },
-        ],
-        filter: ""
-    }
+const MyPhone = () => {
+    const [contacts, setContacts] = useState(()=> {
+        const data = JSON.parse(localStorage.getItem("contacts"));
+        return data || [];
+    });
 
-    componentDidMount() {
-        const contacts = JSON.parse(localStorage.getItem("contacts"));
-        if (contacts?.length) {
-            this.setState({
-                contacts,
-            })
+    const [filter, setFilter] = useState("");
+
+    const firstRender = useRef(true);
+
+    useEffect(()=> {
+        if(!firstRender.current) {
+            localStorage.setItem("contacts", JSON.stringify(contacts));
         }
-    }
+    }, [contacts]);
 
-    componentDidUpdate(prevProps, prevState) {
-        const { contacts } = this.state;
-        if (prevState.contacts.length !== contacts.length) {
-            localStorage.setItem("contacts", JSON.stringify(this.state.contacts));
-        }
-    }
+    useEffect(()=> {
+        firstRender.current = false;
+    }, [])
 
-
-    isDublicate({name}) {
-        const {contacts} = this.state;
+    const isDublicate = ({ name }) => {
         const normalizedName = name.toLowerCase();
-        
+
         const dublicate = contacts.find(item => {
             const normalizedCurrentName = item.name.toLowerCase();
             return (normalizedCurrentName === normalizedName);
@@ -47,48 +37,35 @@ class MyPhone extends Component {
         return Boolean(dublicate);
     }
 
-    addContact = (data) => {        
-        if(this.isDublicate(data)) {
-           return alert(`${data.name} is already in contacts`);
+    const addContact= (data) => {
+        if (isDublicate(data)) {
+            return alert(`${data.name} is already in contacts`);
         }
 
-        this.setState(({contacts}) => {
+        setContacts(prevContacts=> {
             const newContact = {
                 id: nanoid(),
                 ...data,
-            }
+            };
 
-            return {
-                contacts: [...contacts, newContact]
-            }
+            return [...prevContacts, newContact];
         })
     }
 
-    deleteContact = (id)=> {
-        this.setState(({contacts}) => {
-            const newContact = contacts.filter(item => item.id !== id);
-
-            return {
-                contacts: newContact,
-            }
-        })
+    const deleteContact = (id) => {
+        setContacts(prevContacts => prevContacts.filter(item => item.id !== id))
     }
 
-    changeFitler = ({target})=> {
-        this.setState({
-            filter: target.value
-        })
-    }
+    const changeFitler = ({ target }) => setFilter(target.value);
 
-    getFilteredContacts() {
-        const {filter, contacts} = this.state;
-        if(!filter) {
+    const getFilteredContacts = () => {
+        if (!filter) {
             return contacts;
         }
 
         const normalizedFilter = filter.toLowerCase();
 
-        const filteredContacts = contacts.filter(({name}) => {
+        const filteredContacts = contacts.filter(({ name }) => {
             const normalizedName = name.toLowerCase();
 
             return (normalizedName.includes(normalizedFilter))
@@ -97,23 +74,20 @@ class MyPhone extends Component {
         return filteredContacts;
     }
 
-    render() {
-        const {addContact, deleteContact, changeFitler} = this;
-        const contacts = this.getFilteredContacts();
+    const items = getFilteredContacts();
 
-        return (
-            <div className={styles.wrapper}>
-                <h1>Phonebook</h1>
-                <MyPhoneForm className={styles.phoneForm} onSubmit={addContact} />
-                <div className={styles.listWrapper}>
-                    <h1>Contacts</h1>
-                    <p>Find contacts by name</p>
-                    <input className={styles.filter} onChange={changeFitler} name="filter" placeholder="" />
-                    <MyPhoneList items={contacts} deleteContact={deleteContact} />
-                </div>
-            </div>
-        )
-    }
-}
+  return (
+    <div className={styles.wrapper}>
+        <h1>Phonebook</h1>
+        <MyPhoneForm className={styles.phoneForm} onSubmit={addContact} />
+        <div className={styles.listWrapper}>
+            <h1>Contacts</h1>
+            <p>Find contacts by name</p>
+            <input className={styles.filter} onChange={changeFitler} name="filter" placeholder="" />
+            <MyPhoneList items={items} deleteContact={deleteContact} />
+        </div>
+    </div>
+)
+};
 
 export default MyPhone;
--- src/components/MyPhone/MyPhoneForm/MyPhoneForm.jsx
+++ src/components/MyPhone/MyPhoneForm/MyPhoneForm.jsx
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import { useState } from "react";
 import { nanoid } from "nanoid";
 
 import styles from "./my-phoneForm-module.css";
@@ -8,47 +8,45 @@ const INITIAL_STATE = {
     number: "",
 };
 
-class MyPhoneForm extends Component {
-    nameId = nanoid();
-    numberId = nanoid();
+const MyPhoneForm = ({onSubmit}) => {
+    const [state, setState] = useState({...INITIAL_STATE});
 
-    state = {...INITIAL_STATE}
 
-    handleChange = ({target}) => {
+    const handleChange = ({target}) => {
         const {name, value} = target;
-        this.setState({
-            [name]: value
+        setState({
+            ...state,
+            [name]: value,
         })
     }
 
-    handleSubmit = (e)=> {
+    
+    const handleSubmit = (e)=> {
         e.preventDefault();
-        this.props.onSubmit({...this.state});
-        this.reset();
+        onSubmit({...state});
+        reset();
     }
+    const reset = () => {
+        setState({...INITIAL_STATE});
+    }
+    const nameId = nanoid();
+    const numberId = nanoid();
+    const {name, number} = state;
 
-    reset() {
-        this.setState({...INITIAL_STATE});
-    }
-
-    render() {
-        const {nameId, numberId, handleSubmit, handleChange} = this;
-        const {name, number} = this.state;
-        
-        return (
-            <form onSubmit={handleSubmit} className={styles.formPhone}>
-                <div className={styles.formGroup}>  
-                    <label className={styles.phoneLabel} htmlFor={nameId}>Name</label>
-                    <input value={name} type="text" name="name" required onChange={handleChange} id={nameId} placeholder="" />
-                </div>
-                <div className={styles.formGroup}>
-                    <label className={styles.phoneLabel} htmlFor={numberId}>Number</label>
-                    <input value={number} type="tel" name="number" required onChange={handleChange} id={numberId} placeholder="" />
-                </div>
-                <button className={styles.btnSubmit} type="submit">Add contact</button>
-            </form>
-        )
-    }
+    return (
+        <form onSubmit={handleSubmit} className={styles.formPhone}>
+            <div className={styles.formGroup}>  
+                <label className={styles.phoneLabel} htmlFor={nameId}>Name</label>
+                <input value={name} type="text" name="name" required onChange={handleChange} id={nameId} placeholder="" />
+            </div>
+            <div className={styles.formGroup}>
+                <label className={styles.phoneLabel} htmlFor={numberId}>Number</label>
+                <input value={number} type="tel" name="number" required onChange={handleChange} id={numberId} placeholder="" />
+            </div>
+            <button className={styles.btnSubmit} type="submit">Add contact</button>
+        </form>
+    )
 }
+
 
 export default MyPhoneForm;
